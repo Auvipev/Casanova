8ad133c95ce302f2b757544d2bf0a3b4de31be5b
Nicholas <nenglish443@outlook.com>
2018-06-25 20:51:36 -0400

    Create ExceptionInterface.php

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Casanova - A PSR-11 compliant dependency injection container library.
+ */
+
+namespace Casanova\DependencyInjection\Exception;
+
+use Throwable;
+
+/**
+ * @interface ExceptionInterface.
+ * @extends   Throwable
+ */
+interface ExceptionInterface extends Throwable
+{
+}
