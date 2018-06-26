02c09de18d4604d6a7466aad07128d4c1e1b90d4
Nicholas <nenglish443@outlook.com>
2018-06-25 20:30:43 -0400

    Add container class.

---
+++ src/Container.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+/**
+ * Casanova - A PSR-11 compliant dependency injection container library.
+ */
+
+namespace Casanova\DependencyInjection;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * A PSR-11 compliant container.
+ *
+ * @class      Container.
+ * @implements ContainerInterface.
+ */
+class Container implements ContainerInterface
+{
+
+    /**
+     * @var Architect $architect The container architect.
+     */
+    private $architect;
+
+    /**
+     * Bind the architect to the PSR-11 container.
+     *
+     * @param Architect $architect The container architect.
+     *
+     * @return void Returns nothing.
+     *
+     * @codeCoverageIgnore.
+     */
+    public function __construct(Architect $architect)
+    {
+        $this->architect = $architect;
+    }
+
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @throws Exception\IdentifierException No entry was found for **this** identifier.
+     * @throws Exception\ValueException      Error while retrieving the entry.
+     *
+     * @return mixed Entry.
+     */
+    public function get($id)
+    {
+        if ($this->has($id)) {
+            /** $this->architect[$id] Possibly throws Exception\ValueException */
+            return $this->architect[$id];
+        }
+        throw new Exception\IdentifierException(sprintf('No entry was found for `%s` identifier.', $id));
+    }
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * has($id) returning true does not mean that get($id) will not throw an exception.
+     * It does however mean that get($id) will not throw a Exception\IdentifierException.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return bool
+     */
+    public function has($id)
+    {
+        return isset($this->architect[$id]);
+    }
+}
