8114339520cda6d0e5ef4075aed19c91df8fd827
Nicholas <nenglish443@outlook.com>
2018-06-25 21:56:24 -0400

    Update ContainerException.php

--- src/Exception/ContainerException.php
+++ src/Exception/ContainerException.php
@@ -1,1 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Casanova - A PSR-11 compliant dependency injection container library.
+ */
 
+namespace Casanova\DependencyInjection\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+
+/**
+ * @class      ContainerException.
+ * @implements ExceptionInterface.
+ * @implements ContainerExceptionInterface.
+ */
+class ContainerException implements ExceptionInterface, ContainerExceptionInterface
+{
+}
