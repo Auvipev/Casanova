54e26ab5c056a29930b5d5af61d27b3a9804cdc7
Nicholas <nenglish443@outlook.com>
2018-06-27 15:54:53 -0400

    Create ArchitectInterface.php

---
+++ src/ArchitectInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Casanova - A PSR-11 compliant dependency injection container library.
+ */
+
+namespace Casanova\DependencyInjection;
+
+/**
+ * This class is a basic dependency injection architect that gets passed to the
+ * PSR-11 container for management.
+ *
+ * @interface ArchitectInterface.
+ */
+interface ArchitectInterface
+{
+}
